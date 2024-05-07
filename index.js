fcfd69237a4725d491145ca00fd5ed8437b910df
mqlstam <118191161+mqlstam@users.noreply.github.com>
2024-05-07 12:44:40 +0200

    Add voorbeeld repo

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const userRoutes = require('./src/routes/user.routes')
+const logger = require('./src/util/logger')
+
+const app = express()
+
+// express.json zorgt dat we de body van een request kunnen lezen
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+// Dit is een voorbeeld van een simpele route
+app.get('/api/info', (req, res) => {
+    console.log('GET /api/info')
+    const info = {
+        name: 'My Nodejs Express server',
+        version: '0.0.1',
+        description: 'This is a simple Nodejs Express server'
+    }
+    res.json(info)
+})
+
+// Hier komen alle routes
+app.use(userRoutes)
+
+// Route error handler
+app.use((req, res, next) => {
+    next({
+        status: 404,
+        message: 'Route not found',
+        data: {}
+    })
+})
+
+// Hier komt je Express error handler te staan!
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        status: error.status || 500,
+        message: error.message || 'Internal Server Error',
+        data: {}
+    })
+})
+
+app.listen(port, () => {
+    logger.info(`Server is running on port ${port}`)
+})
+
+// Deze export is nodig zodat Chai de server kan opstarten
+module.exports = app
---
+++ src/services/user.service.js
@@ -0,0 +1,70 @@
+const database = require('../dao/inmem-db')
+const logger = require('../util/logger')
+
+const db = require('../dao/mysql-db')
+
+const userService = {
+    create: (user, callback) => {
+        logger.info('create user', user)
+        database.add(user, (err, data) => {
+            if (err) {
+                logger.info(
+                    'error creating user: ',
+                    err.message || 'unknown error'
+                )
+                callback(err, null)
+            } else {
+                logger.trace(`User created with id ${data.id}.`)
+                callback(null, {
+                    message: `User created with id ${data.id}.`,
+                    data: data
+                })
+            }
+        })
+    },
+
+    getAll: (callback) => {
+        logger.info('getAll')
+
+        // Deprecated: de 'oude' manier van werken, met de inmemory database
+        // database.getAll((err, data) => {
+        //     if (err) {
+        //         callback(err, null)
+        //     } else {
+        //         callback(null, {
+        //             message: `Found ${data.length} users.`,
+        //             data: data
+        //         })
+        //     }
+        // })
+
+        // Nieuwe manier van werken: met de MySQL database
+        db.getConnection(function (err, connection) {
+            if (err) {
+                logger.error(err)
+                callback(err, null)
+                return
+            }
+
+            connection.query(
+                'SELECT id, firstName, lastName FROM `user`',
+                function (error, results, fields) {
+                    connection.release()
+
+                    if (error) {
+                        logger.error(error)
+                        callback(error, null)
+                    } else {
+                        logger.debug(results)
+                        callback(null, {
+                            message: `Found ${results.length} users.`,
+                            data: results
+                        })
+                    }
+                }
+            )
+        })
+    }
+}
+
+module.exports = userService
